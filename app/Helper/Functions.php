c60ef6b3da339f149f7d07de7b98ca9103042c6b
zcg <1506411530@qq.com>
2019-07-22 17:15:14 +0800

    upload

--- app/Helper/Functions.php
+++ app/Helper/Functions.php
@@ -1,4 +1,12 @@
 <?php
+
+use W7\App;
+
 /**
  * 公共函数，已加载
  */
+function fileName($name)
+{
+	$uri = App::getApp()->getContext()->getRequest()->getUri();
+	return $uri->getHost().':'.$uri->getPort().'/'.$name;
+}
