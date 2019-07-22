92150a47fd4b4c6dd84b002cf7f72f35d53be3b0
zcg <1506411530@qq.com>
2019-07-22 19:45:41 +0800

    format

--- app/Controller/Admin/UploadController.php
+++ app/Controller/Admin/UploadController.php
@@ -2,40 +2,39 @@
 namespace W7\App\Controller\Admin;
 
 use W7\App\Model\Service\CdnLogic;
-use W7\App\Model\Service\UploadLogic;
 use W7\Http\Message\Server\Request;
 
 class UploadController extends Controller
 {
-	public function image(Request $request)
-	{
-		try {
-			$file = $request->file('icon');
-			if($file){
-				$file = $file->toArray();
-			}else{
-				return $this->error('icon必传');
-			}
+    public function image(Request $request)
+    {
+        try {
+            $file = $request->file('icon');
+            if ($file) {
+                $file = $file->toArray();
+            } else {
+                return $this->error('icon必传');
+            }
 
-			$allowed_mime = ['image/png', 'imagejpg', 'image/gif','image/jpeg'];
-			if($file['error'] != 0){
-				return $this->error('['.$file['error'].']上传失败！网络错误或文件过大');
-			}
-			if (isset($file['type']) && !in_array($file['type'], $allowed_mime)) {
-				return $this->error('only jpg,jpeg,png,gif allowed');
-			}
-			if ($file['size'] > 2*1024*1204){
-				return $this->error('图片尺寸不得超过2M');
-			}
+            $allowed_mime = ['image/png', 'imagejpg', 'image/gif', 'image/jpeg'];
+            if (0 !== $file['error']) {
+                return $this->error('['.$file['error'].']上传失败！网络错误或文件过大');
+            }
+            if (isset($file['type']) && !\in_array($file['type'], $allowed_mime, true)) {
+                return $this->error('only jpg,jpeg,png,gif allowed');
+            }
+            if ($file['size'] > 2 * 1024 * 1204) {
+                return $this->error('图片尺寸不得超过2M');
+            }
 
-			$baseName =md5(time().str_random(10).uniqid());
-			$fileName = $baseName.'.'.explode('/',$file['type'])[1];
-			$cdn = new CdnLogic();
-			$url = $cdn->uploadFile('dc/'.$fileName,$file['tmp_file']);
-			return $this->success(compact('url'));
-		} catch (\Exception $e) {
-			return $this->error($e->getMessage());
-		}
-	}
+            $baseName = md5(time().str_random(10).uniqid());
+            $fileName = $baseName.'.'.explode('/', $file['type'])[1];
+            $cdn = new CdnLogic();
+            $url = $cdn->uploadFile('dc/'.$fileName, $file['tmp_file']);
 
+            return $this->success(compact('url'));
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
+    }
 }
--- app/Controller/Admin/UserAuthorizationController.php
+++ app/Controller/Admin/UserAuthorizationController.php
@@ -13,14 +13,15 @@ class UserAuthorizationController extends Controller
 
     public function index(Request $request)
     {
-        try{
-            if($request->document_user_auth !== APP_AUTH_ALL){
+        try {
+            if (APP_AUTH_ALL !== $request->document_user_auth) {
                 return $this->error('无权访问');
             }
             $user_id = $request->input('user_id');
             $result = $this->logic->getItems($user_id);
+
             return $this->success($result);
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             return $this->error($e->getMessage());
         }
     }
@@ -28,18 +29,19 @@ class UserAuthorizationController extends Controller
     public function update(Request $request)
     {
         //auth = {"document":[{"id":0},{"id":1,"can_read":1,"can_modify":0,"can_delete":0},{"id":2,"can_read":1,"can_modify":1,"can_delete":1}]}
-        try{
-            if($request->document_user_auth !== APP_AUTH_ALL){
+        try {
+            if (APP_AUTH_ALL !== $request->document_user_auth) {
                 return $this->error('无权操作');
             }
-            $auth = json_decode($request->input('auth','{}'),1);
+            $auth = json_decode($request->input('auth', '{}'), 1);
             $user_id = $request->input('user_id');
-            if(!$user_id){
+            if (!$user_id) {
                 return $this->error('缺少参数');
             }
-            $result = $this->logic->updateAuth($user_id,$auth);
-            return $this->success($result,'更新成功');
-        }catch (\Exception $e){
+            $result = $this->logic->updateAuth($user_id, $auth);
+
+            return $this->success($result, '更新成功');
+        } catch (\Exception $e) {
             return $this->error($e->getMessage());
         }
     }
