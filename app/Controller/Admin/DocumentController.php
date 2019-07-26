7b5233aa393d644e078ef171d7587e2a92966e50
wfy <wfy@qq.com>
2019-07-26 09:19:52 +0800

    c

--- app/Controller/Admin/DocumentController.php
+++ app/Controller/Admin/DocumentController.php
@@ -24,6 +24,24 @@ class DocumentController extends Controller
 		$this->user = new UserLogic();
 	}
 
+//	public function get(Request $request)
+//	{
+//		try {
+//			$data = [];
+//			if ($request->input('id')){
+//				$data['id'] = $request->input('id');
+//			}
+//			$res = $this->logic->get($data);
+//			if ($res) {
+//				return $this->success($res);
+//			} else {
+//				return $this->error($res);
+//			}
+//		} catch (\Exception $e) {
+//			return $this->error($e->getMessage());
+//		}
+//	}
+
 	public function create(Request $request)
 	{
 		try {
@@ -37,12 +55,14 @@ class DocumentController extends Controller
 
 			$name = $request->input('name');
 			$username = $request->input('username');
+			$description = $request->input('description');
 
 			$user = $this->user->getUser(['username'=>$username]);
 
 			$data = [];
 			$data['name'] = $name;
 			$data['creator_id'] = $user['id'];
+			$data['description'] = $description;
 
 			$res = $this->logic->create($data);
 			if ($res) {
@@ -55,7 +75,90 @@ class DocumentController extends Controller
 		}
 	}
 
-	public function index()
+	public function update(Request $request)
 	{
+		try {
+			$this->validate($request, [
+				'id' => 'required|integer|min:1',
+				'username' => 'required',
+			], [
+				'id.required' => '文档ID不能为空',
+				'username.required' => '用户名不能为空',
+			]);
+			$username = $request->input('username');
+			$documentId = $request->input('id');
+
+			$relation = $this->relation($username,$documentId);
+			if ($relation !== true){
+				return $this->error($relation);
+			}
+
+			$data = [];
+			if ($request->input('name')){
+				$data['name'] = $request->input('name');
+			}
+			if ($request->input('description')){
+				$data['description'] = $request->input('description');
+			}
+			if ($request->input('url')){
+				$data['url'] = $request->input('url');
+			}
+
+			$res = $this->logic->update($documentId, $data);
+			if ($res) {
+				return $this->success($res);
+			} else {
+				return $this->error($res);
+			}
+		} catch (\Exception $e) {
+			return $this->error($e->getMessage());
+		}
+	}
+
+	public function del(Request $request)
+	{
+		try {
+			$this->validate($request, [
+				'id' => 'required|integer|min:1',
+				'username' => 'required',
+			], [
+				'id.required' => '文档ID不能为空',
+				'username.required' => '用户名不能为空',
+			]);
+			$relation = $this->relation($request->input('username'),$request->input('id'));
+			if ($relation !== true){
+				return $this->error($relation);
+			}
+
+			$res = $this->logic->del($request->input('id'));
+			if ($res) {
+				return $this->success($res);
+			} else {
+				return $this->error($res);
+			}
+		} catch (\Exception $e) {
+			return $this->error($e->getMessage());
+		}
+	}
+
+	private function relation($username,$documentId)
+	{
+		try {
+			$user = $this->user->getUser(['username'=>$username]);
+			$document = $this->logic->get(['id'=>$documentId]);
+			if (!$user){
+				return '用户不存在';
+			}
+			if (!$document){
+				return '文档不存在';
+			}
+			if ($user['id'] != $document['creator_id']){
+				return '只有文档创建者才可以操作';
+			}
+			return true;
+		} catch (\Exception $e) {
+			return $this->error($e->getMessage());
+		}
+
 	}
 }
--- app/Model/Logic/DocumentLogic.php
+++ app/Model/Logic/DocumentLogic.php
@@ -8,8 +8,25 @@ use W7\App\Model\Entity\Document;
 
 class DocumentLogic extends BaseLogic
 {
-	public function add($data)
+	public function get($data)
+	{
+		if (isset($data['id'])){
+			return Document::find($data['id']);
+		}
+	}
+
+	public function create($data)
 	{
 		return Document::create($data);
+	}
+
+	public function update($id, $data)
+	{
+		return Document::where('id', $id)->update($data);
+	}
+
+	public function del($id)
+	{
+		return Document::destroy($id);
 	}
 }
