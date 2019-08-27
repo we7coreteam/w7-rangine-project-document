cee5b6a5655724ccee7f67fa0724d068d42ab0b3
wfy <wfy@w7.cc>
2019-08-27 16:51:41 +0800

    修改文件上传的返回信息格式

--- app/Controller/Admin/UploadController.php
+++ app/Controller/Admin/UploadController.php
@@ -43,9 +43,9 @@ class UploadController extends Controller
 			$cdn = new CdnLogic();
 			$url = $cdn->uploadFile('dc/'.$fileName, $file['tmp_file']);
 
-			return ['state' => 'SUCCESS' ,'success' => 1,'message' => '上传成功','url'=>$url];
+			return $this->success(['state' => 'SUCCESS' ,'success' => 1,'message' => '上传成功','url'=>$url]);
 		} catch (\Exception $e) {
-			return ['success' => 0,'message' => $e->getMessage()];
+			return $this->error(['success' => 0,'message' => $e->getMessage()]);
 		}
 	}
 
