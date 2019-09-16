320690a416f4e6fe44a3c3d8a676b9181b86ffb8
titrxw <1120309488@qq.com>
2019-09-16 17:13:54 +0800

    添加install:init命令获取数据库配置逻辑

--- app/Command/Install/InitCommand.php
+++ app/Command/Install/InitCommand.php
@@ -17,19 +17,14 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use W7\Console\Command\CommandAbstract;
 
-class InitCommand extends CommandAbstract
-{
-	protected function configure()
-	{
-		$this
-			// configure an argument
-			->addArgument('username', InputArgument::REQUIRED, 'The username of the user.')
-			// ...
-		;
-	}
+class InitCommand extends CommandAbstract {
+	protected function handle($options) {
+		$config = $this->getDbConfig();
+		//需要验证数据格式
 
-	protected function handle($options)
-	{
+
+
+//		$output->writeln('Username: ' . $input->getArgument('username'));
 		// 生成.env配置文件
 
 		// 导入SQL数据
@@ -39,14 +34,39 @@ class InitCommand extends CommandAbstract
 		$this->output->writeln('安装成功');
 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$output->writeln([
-			'User Creator',
-			'============',
-			'',
-		]);
+	private function getDbConfig() {
+		$list = [
+			'host' => [
+				'name' => 'host',
+				'default' => '127.0.0.1'
+			],
+			'port' => [
+				'name' => '端口',
+				'default' => '3306'
+			],
+			'name' => [
+				'name' => '数据库名称',
+				'default' => 'we7_document'
+			],
+			'username' => [
+				'name' => '数据库用户名',
+				'default' => 'root'
+			],
+			'password' => [
+				'name' => '数据库密码',
+				'default' => ''
+			]
+		];
 
-		$output->writeln('Username: ' . $input->getArgument('username'));
+		$config = [];
+		foreach ($list as $key => $item) {
+			$config[$key] = $this->askDb($item['name'], $item['default']);
+		}
+
+		return $config;
+	}
+
+	private function askDb($name, $default) {
+		return $this->output->ask('请输入数据库' . $name, $default);
 	}
 }
