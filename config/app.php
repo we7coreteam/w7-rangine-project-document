674870808bb6b7c81d5c617d750849c986e2799c
wfy <wfy@w7.cc>
2019-09-24 17:39:00 +0800

    修改缓存配置

--- config/app.php
+++ config/app.php
@@ -49,9 +49,6 @@ return [
 			'password' => ienv('CACHE_DEFAULT_PASSWORD', ''),
 			'database' => ienv('CACHE_DEFAULT_DATABASE', '0'),
 		],
-		'db' => [
-			'driver' => ienv('CACHE_DEFAULT_DRIVER','db')
-		]
 	],
 	'database' => [
 		'default' => [
