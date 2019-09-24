7ab5663563c189af95308eeeb7e94a1c48e7355e
wfy <wfy@w7.cc>
2019-09-24 17:23:36 +0800

    修改缓存问题

--- app/Subscriber/ClearChapterCacheSubscriber.php
+++ app/Subscriber/ClearChapterCacheSubscriber.php
@@ -16,7 +16,7 @@ class ClearChapterCacheSubscriber extends Subscriber
 {
 	public function run($event)
 	{
-		icache()->channel('db')->delete('chapters_'.$event->chapter['document_id']);
-		icache()->channel('db')->delete('chapter_'.$event->chapter['id']);
+		icache()->delete('chapters_'.$event->chapter['document_id']);
+		icache()->delete('chapter_'.$event->chapter['id']);
 	}
 }
