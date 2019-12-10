68592a7c67e97136be773e9ab52e3bd20012bdd6
titrxw <1120309488@qq.com>
2019-12-10 14:28:28 +0800

    修改文档权限model

---
+++ app/Model/Entity/DocumentPermission.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace W7\App\Model\Entity;
+
+class DocumentPermission extends BaseModel {
+	const MANAGER_PERMISSION = 1;
+	const OPERATOR_PERMISSION = 2;
+	const READER_PERMISSION = 3;
+
+	protected $table = 'document_permission';
+
+	public function asManager() {
+		$this->permission = self::MANAGER_PERMISSION;
+	}
+
+	public function asOperator() {
+		$this->permission = self::OPERATOR_PERMISSION;
+	}
+
+	public function asReader() {
+		$this->permission = self::READER_PERMISSION;
+	}
+
+	public function isManager() : bool {
+		return $this->permission == self::MANAGER_PERMISSION;
+	}
+
+	public function isOperator() : bool {
+		return $this->permission == self::OPERATOR_PERMISSION;
+	}
+
+	public function isReader() : bool {
+		return $this->permission == self::READER_PERMISSION;
+	}
+}
