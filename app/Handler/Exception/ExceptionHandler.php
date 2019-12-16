f363d0509ae64251d679efb1415aaf5dd601f9b4
titrxw <1120309488@qq.com>
2019-12-16 15:57:24 +0800

    修改登录检测逻辑

--- app/Handler/Exception/ExceptionHandler.php
+++ app/Handler/Exception/ExceptionHandler.php
@@ -8,6 +8,7 @@ use W7\Core\Exception\ResponseExceptionAbstract;
 use W7\Core\Exception\RouteNotAllowException;
 use W7\Core\Exception\RouteNotFoundException;
 use W7\Core\Exception\ValidatorException;
+use W7\Core\Session\Session;
 use W7\Core\View\View;
 use W7\Core\Exception\Handler\ExceptionHandler as ExceptionHandlerAbstract;
 
@@ -15,9 +16,11 @@ class ExceptionHandler extends ExceptionHandlerAbstract {
 	public function handle(ResponseExceptionAbstract $e) : ResponseInterface {
 		if ($e instanceof RouteNotFoundException || $e instanceof RouteNotAllowException) {
 			$route = icontext()->getRequest()->getUri()->getPath();
-			if ($route != '/admin/login' && substr($route, 0, 6) == '/admin') {
-				if (!icontext()->getRequest()->session) {
-					return icontext()->getResponse()->redirect('/admin/login');
+			if (substr($route, 0, 6) == '/admin') {
+				$session = new Session();
+				$session->start(icontext()->getRequest());
+				if (!$session->get('user')) {
+					return icontext()->getResponse()->redirect('/login');
 				}
 			}
 			return App::getApp()->getContext()->getResponse()->html(iloader()->singleton(View::class)->render('@public/index'));
