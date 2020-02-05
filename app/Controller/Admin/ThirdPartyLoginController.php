6dab77e24e5af9b676c01c022f91f170bd00bfaa
titrxw <1120309488@qq.com>
2020-02-05 09:45:00 +0800

    添加第三方登录相关api

---
+++ app/Controller/Admin/ThirdPartyLoginController.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * WeEngine Document System
+ *
+ * (c) We7Team 2019 <https://www.w7.cc>
+ *
+ * This is not a free software
+ * Using it under the license terms
+ * visited https://www.w7.cc for more details
+ */
+
+namespace W7\App\Controller\Admin;
+
+use W7\App\Controller\BaseController;
+use W7\App\Exception\ErrorHttpException;
+use W7\App\Model\Logic\ThirdPartyLoginLogic;
+use W7\Http\Message\Server\Request;
+
+class ThirdPartyLoginController extends BaseController
+{
+    private function check(Request $request) 
+    {
+		$user = $request->getAttribute('user');
+		if (!$user->isFounder) {
+			throw new ErrorHttpException('无权访问');
+		}
+		return true;
+    }
+    
+    public function saveThirdPartyLogin(Request $request) 
+    {
+		$this->check($request);
+		$params = $this->validate($request, [
+			'name' => 'required',
+			'logo' => 'required|url',
+			'app_id' => 'required',
+			'secret_key' => 'required',
+			'access_token_url' => 'required|url',
+			'user_info_url' => 'required|url',
+			'login_url_url' => 'required|url',
+		], [
+			'name.required' => 'name必填',
+			'logo.required' => 'logo必传',
+			'app_id.required' => 'app_id必填',
+			'secret_key.required' => 'secret_key必填',
+			'access_token_url.url' => '获取access_token接口地址错误',
+			'user_info_url.url' => '获取用户信息接口地址错误',
+			'login_url_url.url' => '获取登录地址接口地址错误',
+		]);
+		$params['user_info_url'] = rtrim($params['user_info_url'], '/');
+		$params['access_token_url'] = rtrim($params['access_token_url'], '/');
+		$params['login_url_url'] = rtrim($params['login_url_url'], '/');
+		$params['enable'] = !empty($params['enable']) ? true : false;
+		
+		try {
+			ThirdPartyLoginLogic::instance()->addThirdPartyLoginChannel($params);
+			return $this->data('success');
+		} catch (\Throwable $e) {
+			throw new ErrorHttpException($e->getMessage());
+		}
+    }
+    
+    public function getThirdPartyLoginChannelById(Request $request)
+    {
+        $this->check($request);
+        $params = $this->validate($request, [
+			'id' => 'required'
+        ]);
+        try {
+            return $this->data(ThirdPartyLoginLogic::instance()->getThirdPartyLoginChannelById($params['id']));
+        } catch (\Throwable $e) {
+            throw new ErrorHttpException($e->getMessage());
+        }
+    }
+
+    public function updateThirdPartyLoginChannelById(Request $request)
+    {
+        $this->check($request);
+        $params = $this->validate($request, [
+			'id' => 'required',
+			'name' => 'required',
+			'logo' => 'required|url',
+			'app_id' => 'required',
+			'secret_key' => 'required',
+			'access_token_url' => 'required|url',
+			'user_info_url' => 'required|url',
+			'login_url_url' => 'required|url',
+		], [
+			'name.required' => 'name必填',
+			'logo.required' => 'logo必传',
+			'app_id.required' => 'app_id必填',
+			'secret_key.required' => 'secret_key必填',
+			'access_token_url.url' => '获取access_token接口地址错误',
+			'user_info_url.url' => '获取用户信息接口地址错误',
+			'login_url_url.url' => '获取登录地址接口地址错误',
+		]);
+		$params['user_info_url'] = rtrim($params['user_info_url'], '/');
+		$params['access_token_url'] = rtrim($params['access_token_url'], '/');
+		$params['login_url_url'] = rtrim($params['login_url_url'], '/');
+		$params['enable'] = !empty($params['enable']) ? true : false;
+        try {
+            return $this->data(ThirdPartyLoginLogic::instance()->updateThirdPartyLoginChannelById($params['id'], $params));
+        } catch (\Throwable $e) {
+            throw new ErrorHttpException($e->getMessage());
+        }
+    }
+
+    public function thirdPartyLoginChannel(Request $request) 
+    {
+		$this->check($request);
+		return $this->data(ThirdPartyLoginLogic::instance()->getThirdPartyLoginChannel());
+    }
+    
+    public function setDefaultLoginChannel(Request $request) {
+        $this->check($request);
+		$params = $this->validate($request, [
+			'default_login_channel' => 'required'
+		]);
+
+		ThirdPartyLoginLogic::instance()->setDefaultLoginChannel($params['default_login_channel']);
+		return $this->data('success');
+    }
+}
---
+++ app/Model/Logic/ThirdPartyLoginLogic.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * WeEngine Document System
+ *
+ * (c) We7Team 2019 <https://www.w7.cc>
+ *
+ * This is not a free software
+ * Using it under the license terms
+ * visited https://www.w7.cc for more details
+ */
+
+namespace W7\App\Model\Logic;
+
+use W7\Core\Helper\Traiter\InstanceTraiter;
+
+class ThirdPartyLoginLogic extends BaseLogic
+{
+	use InstanceTraiter;
+	const THIRD_PARTY_LOGIN_SETTING_KEY = 'third_party_login';
+
+	public function __construct()
+	{
+		$setting = SettingLogic::instance()->getByKey(self::THIRD_PARTY_LOGIN_SETTING_KEY);
+		if (empty($setting['channel'])) {
+			$setting['channel'] = [
+				[
+					'name' => 'QQ'
+				],
+				[
+					'name' => '微信'
+				]
+			];
+			SettingLogic::instance()->save(self::THIRD_PARTY_LOGIN_SETTING_KEY, $setting);
+		}
+	}
+
+    public function getThirdPartyLoginSetting()
+    {
+		$setting = SettingLogic::instance()->getByKey(self::THIRD_PARTY_LOGIN_SETTING_KEY);
+		if (!$setting) {
+			return [];
+		}
+
+		return $setting;
+	}
+
+    public function getThirdPartyLoginChannel()
+    {
+		$setting = $this->getThirdPartyLoginSetting();
+		$channel = $setting['channel'] ?? [];
+
+		return array_column($channel, 'name');
+	}
+
+    public function getThirdPartyLoginChannelByName($name)
+    {
+		$setting = $this->getThirdPartyLoginSetting();
+		$nameArr = array_column($setting['channel'], 'name');
+		$index = array_search($name, $nameArr);
+		if ($index === false) {
+			return false;
+		}
+
+		return $setting['channel'][$index];
+	}
+
+    public function getThirdPartyLoginChannelById($id)
+    {
+		$setting = $this->getThirdPartyLoginSetting();
+		return $setting['channel'][$id - 1] ?? [];
+	}
+	
+    public function addThirdPartyLoginChannel(array $config)
+    {
+		$setting = $this->getThirdPartyLoginSetting();
+		$nameArr = array_column($setting['channel'], 'name');
+		$index = array_search($config['name'], $nameArr);
+		if ($index !== false) {
+			throw new \RuntimeException('该授权方式名称已存在');
+		}
+
+		$setting['channel'][] = $config;
+		SettingLogic::instance()->save(self::THIRD_PARTY_LOGIN_SETTING_KEY, $setting);
+	}
+	
+    public function updateThirdPartyLoginChannelById($id, array $config)
+    {
+		$setting = $this->getThirdPartyLoginSetting();
+		if (empty($setting['channel'][$id - 1])) {
+			throw new \RuntimeException('该授权方式不存在');
+		}
+		$nameArr = array_column($setting['channel'], 'name');
+		$index = array_search($config['name'], $nameArr);
+		if ($index !== false && $index != $id - 1) {
+			throw new \RuntimeException('该授权方式名称已存在');
+		}
+		$setting['channel'][$id] = $config;
+		SettingLogic::instance()->save(self::THIRD_PARTY_LOGIN_SETTING_KEY, $setting);
+	}
+
+	public function setDefaultLoginChannel($channelName)
+	{
+		$setting = $this->getThirdPartyLoginSetting();
+		$setting['default_login_channel'] = $channelName;
+		SettingLogic::instance()->save(self::THIRD_PARTY_LOGIN_SETTING_KEY, $setting);
+	}
+}
