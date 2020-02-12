2ac1d37accc8480f9eff260deb46d1ba89ff5005
titrxw <1120309488@qq.com>
2020-02-13 04:16:48 +0800

    调整qq登录

--- app/Provider/Socialite/ThirdPartyLogin/QQOauth.php
+++ app/Provider/Socialite/ThirdPartyLogin/QQOauth.php
@@ -3,6 +3,7 @@
 namespace W7\App\Provider\Socialite\ThirdPartyLogin;
 
 use Overtrue\Socialite\Providers\QQProvider;
+use Overtrue\Socialite\AccessTokenInterface;
 
 class QQOauth extends QQProvider
 {
@@ -21,5 +22,29 @@ class QQOauth extends QQProvider
     protected function getTokenUrl()
     {
         return parent::getTokenUrl();
+    }
+
+    protected function getUserByToken(AccessTokenInterface $token)
+    {
+        $url = $this->baseUrl.'/oauth2.0/me?access_token='.$token->getToken();
+        $this->withUnionId && $url .= '&unionid=1';
+
+        $response = $this->getHttpClient()->get($url);
+
+        $me = json_decode($this->removeCallback($response->getBody()->getContents()), true);
+        $this->openId = $me['openid'];
+        $this->unionId = isset($me['unionid']) ? $me['unionid'] : '';
+
+        $queries = [
+            'access_token' => $token->getToken(),
+            'openid' => $this->openId,
+            'oauth_consumer_key' => $this->clientId,
+        ];
+
+        $response = $this->getHttpClient()->get($this->baseUrl.'/user/get_user_info?'.http_build_query($queries));
+        
+        $info = json_decode($this->removeCallback($response->getBody()->getContents()), true);
+        $info['openid'] = $this->openId;
+        return $info;
     }
 }
