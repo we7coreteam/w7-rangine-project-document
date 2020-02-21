981d5c2d6faf9263738fd0eabfd1bf7a9e8f289f
titrxw <1120309488@qq.com>
2020-02-21 11:07:48 +0800

    处理数据不完整导致的错误

--- app/Controller/Admin/DocumentController.php
+++ app/Controller/Admin/DocumentController.php
@@ -58,8 +58,8 @@ class DocumentController extends BaseController
 							'name' => $row->user->username
 						],
 						'operator' => [
-							'name' => $lastOperate->operateDesc,
-							'time' => $lastOperate->created_at
+							'name' => !empty($lastOperate) ? $lastOperate->operateDesc : '',
+							'time' => !empty($lastOperate) ? $lastOperate->created_at->toDateTimeString() : ''
 						],
 						'has_star' => $star ? true : false,
 						'description' => $row->descriptionShort,
@@ -96,8 +96,8 @@ class DocumentController extends BaseController
 							'name' => $row->document->user->username
 						],
 						'operator' => [
-							'name' => $lastOperate->operateDesc,
-							'time' => $lastOperate->created_at
+							'name' => !empty($lastOperate) ? $lastOperate->operateDesc : '',
+							'time' => !empty($lastOperate) ? $lastOperate->created_at->toDateTimeString() : ''
 						],
 						'has_star' => $star ? true : false,
 						'description' => $row->document->descriptionShort,
