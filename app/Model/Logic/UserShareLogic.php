9edcb9ec6a291ff9c85b84b7871586487e3d5391
titrxw <1120309488@qq.com>
2020-03-05 13:00:40 +0800

    修改share key 默认值

--- app/Model/Logic/UserShareLogic.php
+++ app/Model/Logic/UserShareLogic.php
@@ -8,16 +8,16 @@ class UserShareLogic extends BaseLogic
 {
 	use InstanceTraiter;
 
-	const SHARE_KEY = '';
+	const SHARE_KEY = 'e8598e0ed61835892a79acdffa7f4f95';
 
 	public function getShareUrl($userId, $documentId, $chapterId)
 	{
-		return rtrim(ienv('API_HOST'), '/') . '/chapter/' . $documentId. '?id=' . $chapterId . '&share_key=' . base64_encode(authcode($userId . '-' . $chapterId, 'ENCODED', self::SHARE_KEY));
+		return rtrim(ienv('API_HOST'), '/') . '/chapter/' . $documentId. '?id=' . $chapterId . '&share_key=' . authcode($userId . '-' . $chapterId, 'ENCODED', self::SHARE_KEY);
 	}
 
 	public function getUidAndChapterByShareKey($shareKey)
 	{
-		$data = base64_decode(authcode($shareKey, 'DECODE', self::SHARE_KEY));
+		$data = authcode($shareKey, 'DECODE', self::SHARE_KEY);
 		$data = explode('-', $data);
 		if (count($data) != 2) {
 			throw new \RuntimeException('share key error');
