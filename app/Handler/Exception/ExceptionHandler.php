2bb9c80689722664a54ca0ac8e99d05f4b8bd360
titrxw <1120309488@qq.com>
2020-03-06 14:16:56 +0800

    添加注释

--- app/Handler/Exception/ExceptionHandler.php
+++ app/Handler/Exception/ExceptionHandler.php
@@ -42,6 +42,7 @@ class ExceptionHandler extends ExceptionHandlerAbstract {
 					$documentId = explode('/', $route)[2] ?? '';
 					$documentId = explode('?', $documentId)[0];
 					$document = App\Model\Logic\DocumentLogic::instance()->getById($documentId);
+					//非公有文档，自动跳转登录
 					if ($document && $document->is_public != App\Model\Entity\Document::PUBLIC_DOCUMENT) {
 						return icontext()->getResponse()->redirect($this->getLoginUrl(ienv('API_HOST') . ltrim($route, '/')));
 					}
