d40e05e27899131046b1c0675c7b34d882d52c99
凌乐 <563384868@qq.com>
2020-04-17 16:40:20 +0800

    HTTP文档-结构调整

--- app/Model/Logic/Document/ChapterApi/ChapterRecordLogic.php
+++ app/Model/Logic/Document/ChapterApi/ChapterRecordLogic.php
@@ -429,13 +429,13 @@ class ChapterRecordLogic
 					$val->children = $this->getBodyChildren($chapterId, $val->id);
 					if ($val->location == $chapterApi->body_param_location) {
 						//如果当前列是request_body
-						$record['body']['request_body'] = $val;
+						$record['body']['request_body'][] = $val;
 					}
 					if ($val->location == ChapterApiParam::LOCATION_REPONSE_BODY_RAW) {
 						//如果当前列是reponse_body
-						$record['body']['reponse_body'] = $val;
+						$record['body']['reponse_body'][] = $val;
 					} else {
-						$record['body'][$val->location] = $val;
+						$record['body'][$val->location][] = $val;
 					}
 				}
 //				ksort($body);
