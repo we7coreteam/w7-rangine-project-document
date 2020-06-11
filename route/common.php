91bc3ad82571e9a9b0ba692f2aff89de4a1c071c
凌乐 <563384868@qq.com>
2020-06-11 14:05:28 +0800

    退出登陆跨域

--- route/common.php
+++ route/common.php
@@ -28,6 +28,7 @@ irouter()->post('/common/auth/login', 'Common\AuthController@login');
 irouter()->post('/common/auth/method', 'Common\AuthController@method');
 
 irouter()->get('/common/auth/getlogouturl', 'Common\AuthController@getlogouturl');
+irouter()->get('/common/auth/logout', 'Common\AuthController@logout');
 irouter()->post('/common/auth/logout', 'Common\AuthController@logout');
 irouter()->middleware('CheckAuthMiddleware')
 	->post('/common/auth/user', 'Common\AuthController@user');
