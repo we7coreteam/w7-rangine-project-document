e02c63e368d11a74a377daa3d8f9447b9b6dd03f
凌乐 <563384868@qq.com>
2020-07-10 18:01:48 +0800

    安装初始化

--- mock/app.js
+++ mock/app.js
@@ -4,16 +4,19 @@ const port = 3000
 
 const generateData = (data) => {
 	console.log(data);
-	if(data.query&&data.query.record){
-		var json=data.query.record;
-		var obj = eval('(' + json + ')');
-		var Mock = require('mockjs')
-		var mockData = Mock.mock(obj)
-		return mockData;
-	}
-	return [];
+	// if(data.query&&data.query.record){
+	// 	var json=data.query.record;
+	// 	var obj = eval('(' + json + ')');
+	// 	var Mock = require('mockjs')
+	// 	var mockData = Mock.mock(obj)
+	// 	return mockData;
+	// }
+	return {query:data.query,body:data.body};
 }
+var bodyp=require('body-parser');
+//post中间件接收数据
+app.use(bodyp.urlencoded({ extended: false,limit:20*1024})); //extended 拓展模式  limit 最大接收数据
 
-app.post('/buildMock', (req, res) => res.json(generateData(req)))
+app.all('/buildMock', (req, res, next) => res.json(generateData(req)))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
