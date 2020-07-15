c81b7db6bd8c799c22cceaff0cf9a500c7e611b2
凌乐 <563384868@qq.com>
2020-07-15 16:49:22 +0800

    安装初始化

--- app/Controller/Install/IndexController.php
+++ app/Controller/Install/IndexController.php
@@ -13,6 +13,7 @@
 namespace W7\App\Controller\Install;
 
 use W7\App\Controller\BaseController;
+use W7\App\Exception\ErrorHttpException;
 use W7\App\Model\Logic\Install\InstallLogic;
 use W7\Http\Message\Server\Request;
 
@@ -30,10 +31,10 @@ class IndexController extends BaseController
 	{
 		$diskfreespace = diskfreespace(BASE_PATH);
 		$diskfreespaceG = (ceil($diskfreespace / 1000 / 1000 / 10) / 100);
-		$isInstall=file_exists(RUNTIME_PATH . '/install.lock');
+		$isInstall = file_exists(RUNTIME_PATH . '/install.lock');
 		if ($isInstall) {
 			$data = [
-				['id' => 1, 'name' => '已有安装记录', 'result' =>$isInstall ? '文档系统已经安装，如果需要重新安装请手动删除 runtime/install.lock 文件' : '未安装', 'enable' => $isInstall ? true : false],
+				['id' => 1, 'name' => '已有安装记录', 'result' => $isInstall ? '文档系统已经安装，如果需要重新安装请手动删除 runtime/install.lock 文件' : '未安装', 'enable' => $isInstall ? true : false],
 			];
 		} else {
 			$data = [
@@ -59,18 +60,15 @@ class IndexController extends BaseController
 	 * @apiGroup install
 	 *
 	 * @apiParam {String} api_host 服务器地址
-	 * @apiParam {Number} server_port 服务器端口号
 	 * @apiParam {String} db_database 数据库名称
 	 * @apiParam {String} db_host 数据库地址
-	 * @apiParam {Number} db_port 数据库端口
 	 * @apiParam {String} db_username 数据库用户名
 	 * @apiParam {String} db_password 数据库密码
 	 * @apiParam {String} db_prefix 数据库表前缀
 	 * @apiParam {String} admin_username 管理员账户
 	 * @apiParam {String} admin_password 管理员密码
-	 * @apiParam {String} cache_driver 缓存驱动 选项：default、redis
+	 * @apiParam {String} cache_driver 缓存驱动 选项：redis
 	 * @apiParam {String} cache_host 缓存服务器地址（redis时填写）
-	 * @apiParam {String} cache_port 缓存服务器端口号（redis时填写）
 	 *
 	 * @apiSuccessExample {json} Success-Response:
 	 * {"status":true,"code":200,"data":"安装已完成！提示：请按照文档配置，重启相关服务","message":"ok"}
@@ -79,33 +77,44 @@ class IndexController extends BaseController
 	{
 		$params = $this->validate($request, [
 			'api_host' => 'required',
-			'server_port' => 'required',
 			'db_database' => 'required',
 			'db_host' => 'required',
-			'db_port' => 'required',
 			'db_username' => 'required',
 			'db_password' => 'required',
 			'db_prefix' => 'required',
 			'admin_username' => 'required',
 			'admin_password' => 'required',
-			'cache_driver' => 'string',
 			'cache_host' => 'string',
-			'cache_port' => 'integer',
 		], [
 			'api_host' => '服务器地址',
-			'server_port' => '服务器端口',
 			'db_database' => '数据库',
 			'db_host' => '数据库地址',
-			'db_port' => '数据库端口',
 			'db_username' => '数据库用户名',
 			'db_password' => '数据库密码',
 			'db_prefix' => '数据库表前缀',
 			'admin_username' => '管理员密码',
 			'admin_password' => '管理员账户',
-			'cache_driver' => '缓存驱动',
 			'cache_host' => '缓存服务器',
-			'cache_port' => '缓存端口',
 		]);
+		$apiHost = explode(':', $params['api_host']);
+		if (count($apiHost) < 2) {
+			throw new ErrorHttpException('请填写端口号');
+		}
+		$params['server_port'] = $apiHost[1];
+
+		$dbHost = explode(':', $params['db_host']);
+		if (count($dbHost) < 2) {
+			throw new ErrorHttpException('请填写数据库端口号');
+		}
+		$params['db_port'] = $dbHost[1];
+
+		$params['cache_driver'] = 'redis';
+		$cacheHost = explode(':', $params['cache_host']);
+		if (count($cacheHost) < 2) {
+			throw new ErrorHttpException('请填写redis端口号');
+		}
+		$params['cache_port'] = $cacheHost[1];
+
 		$installLogic = new InstallLogic();
 		$data = $installLogic->install($params);
 		return $this->data($data);
