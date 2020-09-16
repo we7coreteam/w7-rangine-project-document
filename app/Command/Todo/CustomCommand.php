964e091759e2ef216d18d214726a69df8f924bca
凌乐 <563384868@qq.com>
2020-09-16 17:55:28 +0800

    空项目处理

--- app/Command/Todo/CustomCommand.php
+++ app/Command/Todo/CustomCommand.php
@@ -13,6 +13,7 @@
 namespace W7\App\Command\Todo;
 
 use W7\App\Model\Entity\Document;
+use W7\App\Model\Entity\UserThirdParty;
 use W7\Console\Command\CommandAbstract;
 
 class CustomCommand extends CommandAbstract
@@ -27,6 +28,7 @@ class CustomCommand extends CommandAbstract
 	protected function handle($options)
 	{
 		$this->updateDocumentNull();
+		$this->updateDoc();
 	}
 
 	public function updateDocumentNull()
@@ -41,10 +43,76 @@ class CustomCommand extends CommandAbstract
 				$l = Document\Chapter::query()->where('document_id', $val->id)->get();
 				if (!count($l)) {
 					$val->delete();
-					$this->output->writeln($val->id . '空项目' . $val->name . '已处理');
+					$this->output->writeln($val->id . '空项目《' . $val->name . '》已处理');
 				}
 			}
 		}
-		$this->output->writeln('已处理结束');
+		$this->output->writeln('空项目已处理结束');
+	}
+
+	public function updateDoc()
+	{
+		$str = '"uid","wiki_id"
+"76052","75"
+"259806","76"
+"76052","77"
+"76052","79"
+"259806","80"
+"75780","81"
+"75780","82"
+"75780","83"
+"75780","84"
+"76052","94"
+"348483","144"
+"386935","162"
+"76052","177"
+"76052","268"
+"209752","280"
+"348483","405"
+"328372","410"
+"348483","447"
+"209752","553"
+"348483","701"
+"37803","912"
+"37803","915"
+"328372","932"
+"269786","962"
+"386935","1026"
+"348483","1071"
+';
+		$str = str_replace('"', '', $str);
+		$str = str_replace('"', '', $str);
+		$data = explode("\n", $str);
+
+		$data1 = [];
+		foreach ($data as $key => $val) {
+			if ($val) {
+				$new = explode(',', $val);
+				if (is_numeric($new[0])) {
+					$oldUser = 0;
+					$newUser = 0;
+					$row = Document::query()->find($new[1]);
+					if ($row) {
+						$oldUser = $row->creator_id;
+					}
+					$userThirdParty = UserThirdParty::query()->where('openid', $new[0])->first();
+					if ($userThirdParty) {
+						$newUser = $userThirdParty->uid;
+					}
+					$data1[count($data1)] = [
+						'openid' => $new[0],
+						'document_id' => $new[1],
+						'old_user' => $oldUser,
+						'new_user' => $newUser
+					];
+					if ($oldUser == 37 && $newUser) {
+						$row->creator_id = $newUser;
+						$row->save();
+						$this->output->writeln('《'.$row->name.'》:用户归属已调整');
+					}
+				}
+			}
+		}
+		$this->output->writeln('用户归属已处理结束');
 	}
 }
