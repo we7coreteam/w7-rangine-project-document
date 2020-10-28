6926d2249fface99a5b342a2f665c3485c99b656
凌乐 <563384868@qq.com>
2020-10-28 11:50:10 +0800

    删除稳定验证名称

--- app/Controller/Admin/DocumentController.php
+++ app/Controller/Admin/DocumentController.php
@@ -84,10 +84,16 @@ class DocumentController extends BaseController
 			if ($role == 1) {
 				$permissions = [DocumentPermission::MANAGER_PERMISSION];
 			}
-			//$role为2代表为参与者
+
+			//$role为2代表为参与者（参与与创建）
 			if ($role == 2) {
+				$permissions = [DocumentPermission::MANAGER_PERMISSION, DocumentPermission::OPERATOR_PERMISSION];
+			}
+			//$role为3代表为仅参与
+			if ($role == 3) {
 				$permissions = [DocumentPermission::OPERATOR_PERMISSION];
 			}
+
 			if ($onlyRead) {
 				$permissions = [DocumentPermission::READER_PERMISSION];
 			}
