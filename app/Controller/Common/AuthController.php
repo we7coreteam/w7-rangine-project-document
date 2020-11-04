58aa2877743acdd338777bb6b65e23220a0e0c8a
凌乐 <563384868@qq.com>
2020-11-04 18:56:28 +0800

    三方登陆

--- app/Controller/Common/AuthController.php
+++ app/Controller/Common/AuthController.php
@@ -187,6 +187,8 @@ class AuthController extends BaseController
 			throw new ErrorHttpException('app_id错误');
 		}
 
+
+
 		$setting = ThirdPartyLoginLogic::instance()->getThirdPartyLoginChannelById($appId);
 		if (!$setting) {
 			throw new ErrorHttpException('不支持该授权方式');
@@ -243,15 +245,96 @@ class AuthController extends BaseController
 		//已登陆的用户校验是否需要切换用户S
 		$user = $request->session->get('user');
 		if ($user) {
-			if ($thirdPartyUser->uid != $user['uid']) {
-				//如果登陆用户和当前访问用户不一致
-				$changeToken = 'temp_user_info' . date('YmdHis') . round(1000, 9999);
-				icache()->set($changeToken, ['third_party_user_id' => $thirdPartyUser->id], 60 * 15);
-				return $this->data(['has_login' => true, 'change_token' => $changeToken]);
+			$LoginUserthirdParty = UserThirdParty::query()
+				->where('uid', $user['uid'])
+				->where('source', $appId)->first();
+			if ($LoginUserthirdParty) {
+				//如果当前登陆账户，已经绑定了第三方-账户一致，返回成功，账户不一致，提示切换
+				if ($thirdPartyUser->uid) {//商城绑了文档
+					if ($thirdPartyUser->uid != $user['uid']) {
+						//4如果登陆用户和当前访问用户不一致
+						$changeToken = 'temp_user_info' . date('YmdHis') . round(1000, 9999);
+						icache()->set($changeToken, ['third_party_user_id' => $thirdPartyUser->id], 60 * 15);
+						return $this->data(['has_login' => 1, 'change_token' => $changeToken]);
+					}
+				} elseif (!$thirdPartyUser->uid) {
+					//文档绑了商城，商城没有绑文档-去登陆
+					$sourceToken = 'temp_user_info_source' . date('YmdHis') . round(1000, 9999);
+					icache()->set($sourceToken, ['third_party_user_id' => $thirdPartyUser->id, 'source' => $appId], 60 * 15);
+					return $this->data(['has_login' => 3, 'source_token' => $sourceToken]);
+				}
+			} else {
+				//1当前登陆账户，没有绑定第三方-登陆账户也没有绑定第三方。商城没有绑文档，文档没有绑商城
+				if (!$thirdPartyUser->uid) {
+					//1文档没有绑商城，商城没有绑文档，如果切入用户，没有绑定账户，当前用户也没有绑定
+					$bindToken = 'temp_user_info_unbind_two' . date('YmdHis') . round(1000, 9999);
+					icache()->set($bindToken, ['third_party_user_id' => $thirdPartyUser->id], 60 * 15);
+					return $this->data(['has_login' => 2, 'bind_token' => $bindToken]);
+				} else {
+					//3文档没有绑商城，商城绑了文档-去切换
+					$changeToken = 'temp_user_info' . date('YmdHis') . round(1000, 9999);
+					icache()->set($changeToken, ['third_party_user_id' => $thirdPartyUser->id], 60 * 15);
+					return $this->data(['has_login' => 1, 'change_token' => $changeToken]);
+				}
 			}
 		}
-		//已登陆的用户校验是否需要切换用户E
+		/*0如果没有登陆，没有绑账户去绑定、
+		---如果登陆了
+		 *1文档没有绑商城，商城没有绑文档-去绑定
+		 *3文档没有绑商城，商城绑了文档-去切换
+		 *2文档绑了商城，商城没有绑文档-去登陆
+		 *4文档绑了商城，商城绑了文档，用户不一致-去切换-一致登陆
+		 **/
+		//2已登陆的用户校验是否需要切换用户E
 		return $this->data($this->setThirdPartySession($request, $thirdPartyUser));
+	}
+
+	/**
+	 * @api {post} /common/auth/ThirdPartyUserCache 文档绑了商城，商城没有绑文档切用户
+	 *
+	 * @apiName ThirdPartyUserCache
+	 * @apiGroup auth
+	 *
+	 * @apiParam {string} bind_token 用于绑定的bind_token
+	 */
+	public function ThirdPartyUserCache(Request $request)
+	{
+		$user = $request->session->get('user');
+		if (!$user) {
+			throw new ErrorHttpException('当前账户未登陆');
+		}
+		$changeToken = $request->input('bind_token');
+		if (empty($changeToken)) {
+			throw new ErrorHttpException('bind_token错误');
+		}
+
+		$data = icache()->get($changeToken);
+		if (isset($data['third_party_user_id'])) {
+			$thirdPartyUser = UserThirdParty::query()->find($data['third_party_user_id']);
+			if ($thirdPartyUser) {
+				$username = $thirdPartyUser->username;
+				$localUser = [
+					'app_id' => $thirdPartyUser->source,
+					'uid' => $thirdPartyUser->uid,
+					'third-party-uid' => $thirdPartyUser->id,
+					'username' => $username,
+				];
+
+				$request->session->destroy();
+				//记录第三方登录app_id
+				$request->session->set('user-source-app', $thirdPartyUser->source);
+
+				//需要绑定已有账户
+				if (!empty($loginSetting['is_need_bind']) && empty($thirdPartyUser->uid)) {
+					//保存第三方用户信息，触发用户绑定
+					$request->session->set('third-party-user', $localUser);
+					return [
+						'is_need_bind' => true
+					];
+				}
+			}
+		}
+		throw new ErrorHttpException('source_token已过期');
 	}
 
 	/**
@@ -280,6 +363,38 @@ class AuthController extends BaseController
 			}
 		}
 		throw new ErrorHttpException('change_token已过期');
+	}
+
+	/**
+	 * @api {post} /common/auth/bindThirdPartyUser 切换用户
+	 *
+	 * @apiName bindThirdPartyUser
+	 * @apiGroup auth
+	 *
+	 * @apiParam {string} bind_token 用于绑定的bind_token
+	 */
+	public function bindThirdPartyUser(Request $request)
+	{
+		$user = $request->session->get('user');
+		if (!$user) {
+			throw new ErrorHttpException('当前账户未登陆');
+		}
+		$changeToken = $request->input('bind_token');
+		if (empty($changeToken)) {
+			throw new ErrorHttpException('bind_token错误');
+		}
+		$data = icache()->get($changeToken);
+		if (isset($data['third_party_user_id'])) {
+			$thirdPartyUser = UserThirdParty::query()->find($data['third_party_user_id']);
+			if ($thirdPartyUser) {
+				if (!$thirdPartyUser->uid) {
+					$thirdPartyUser->uid = $user['uid'];
+					$thirdPartyUser->save();
+					return $this->data($this->setThirdPartySession($request, $thirdPartyUser));
+				}
+			}
+		}
+		throw new ErrorHttpException('bind_token已过期');
 	}
 
 	public function setThirdPartySession(Request $request, $thirdPartyUser)
