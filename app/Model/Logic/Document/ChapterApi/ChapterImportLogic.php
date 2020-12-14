1cf13d94642dcb6f52d821802b635638800ea0e5
凌乐 <563384868@qq.com>
2020-12-14 09:42:08 +0800

    导入数组默认值去重

--- app/Model/Logic/Document/ChapterApi/ChapterImportLogic.php
+++ app/Model/Logic/Document/ChapterApi/ChapterImportLogic.php
@@ -106,7 +106,7 @@ class ChapterImportLogic extends ChapterCommonLogic
 			if (is_array($v) && $type == ChapterApiParam::TYPE_ARRAY) {
 				//如果子数组是个数组
 				$rule = $rule + 1;
-				$sunArray = array_merge_recursive($sunArray, $v);
+				$sunArray = array_unique(array_merge_recursive($sunArray, $v));
 			}
 		}
 		if ($type == ChapterApiParam::TYPE_OBJECT) {
