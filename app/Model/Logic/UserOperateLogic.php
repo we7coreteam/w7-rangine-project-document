2ebff439e831d60a5dedbfb681c53fc6f951f677
wangcb615@163.com <wangcb4718>
2021-04-12 14:55:27 +0800

    优化

--- app/Model/Logic/UserOperateLogic.php
+++ app/Model/Logic/UserOperateLogic.php
@@ -2,6 +2,7 @@
 
 namespace W7\App\Model\Logic;
 
+use phpDocumentor\Reflection\DocBlock\Tags\Uses;
 use W7\App\Model\Entity\UserOperateLog;
 use W7\Core\Helper\Traiter\InstanceTraiter;
 
@@ -24,13 +25,22 @@ class UserOperateLogic extends BaseLogic
 		return UserOperateLog::query()->where('document_id', '=', $documentId)->delete();
 	}
 
-	public function lists($where = [], $page = 1, $size = 20){
-		$query = UserOperateLog::query()->with(['user','document','document.user'=>function($query){
-			$query->select(['id','username','avatar']);
-		}]);
+	public function lists($where = [], $page = 1, $size = 20, $hasCreateChapter = false){
+		$query = UserOperateLog::query()
+			->with(['user','document','document.user'=>function($query){
+				$query->select(['id','username','avatar']);
+			}]);
 		if (!empty($where['user_id']))	$query->where('user_id',$where['user_id']);
-		if (!empty($where['operate']))	$query->whereIn('operate',$where['operate']);
+		if (!empty($where['operate'])) {
+			!$hasCreateChapter && in_array(UserOperateLog::CREATE, $where['operate'])
+				? $query->where(function ($query){
+					$query->where('operate',UserOperateLog::CREATE)->where('chapter_id',0);
+				})->orWhere(function ($query) use($where){
+					$query->whereIn('operate',array_diff($where['operate'],[UserOperateLog::CREATE]));
+				})
+				: $query->whereIn('operate',$where['operate']);
+		}
 
-		return $query->paginate($size,['*'],'',$page);
+		return $query->orderBy('id','desc')->paginate($size,['*'],'',$page);
 	}
 }
