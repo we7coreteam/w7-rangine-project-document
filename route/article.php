8de71fcaf4c38436883d29babd2c2a231b0ad76e
凌乐 <563384868@qq.com>
2021-04-21 08:31:23 +0800

    文章

--- route/article.php
+++ route/article.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * WeEngine Document System
+ *
+ * (c) We7Team 2019 <https://www.w7.cc>
+ *
+ * This is not a free software
+ * Using it under the license terms
+ * visited https://www.w7.cc for more details
+ */
+
 irouter()->middleware(['CheckAuthMiddleware'])->group([], function (\W7\Core\Route\Route $route) {
 	//系统标签配置
 	$route->get('/article/articleTagConfig', 'Article\ArticleTagConfigController@index');
@@ -12,8 +22,8 @@ irouter()->middleware(['CheckAuthMiddleware'])->group([], function (\W7\Core\Rou
 
 	//文章
 	$route->get('/article', 'Article\ArticleController@index');
+	$route->get('/article/indexMy', 'Article\ArticleController@indexMy');
 	$route->get('/article/{id:\d+}', 'Article\ArticleController@show');
 	$route->post('/article', 'Article\ArticleController@store');
 	$route->put('/article/{id:\d+}', 'Article\ArticleController@update');
 });
-
