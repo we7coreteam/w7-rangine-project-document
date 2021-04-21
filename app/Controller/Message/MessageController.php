db2ee0343392c1c2c9b4d8f85a667388387eda4e
凌乐 <563384868@qq.com>
2021-04-21 15:29:21 +0800

    消息

--- app/Controller/Message/MessageController.php
+++ app/Controller/Message/MessageController.php
@@ -31,8 +31,11 @@ class MessageController extends BaseController
 	 * @apiParam {String} target_type 消息类型
 	 * @apiParam {Number} is_read 状态1已读2未读
 	 *
+	 * @apiSuccess {Object} text 消息内容对象
+	 * @apiSuccess {String} text.content 消息内容
+	 *
 	 * @apiSuccessExample {json} Success-Response:
-	 * {"status":true,"code":200,"data":{"current_page":1,"data":[{"id":4,"column_id":1,"tag_ids":["1","2"],"user_id":1,"title":"222","content":"111","comment_status":1,"is_reprint":1,"reprint_url":"","home_thumbnail":1,"read_num":0,"praise_num":0,"status":0,"reason":"","created_at":"1618912571","updated_at":"1618912571","status_text":"待审核"}],"first_page_url":"\/?=1","from":1,"last_page":4,"last_page_url":"\/?=4","next_page_url":"\/?=2","path":"\/","per_page":"1","prev_page_url":null,"to":1,"total":4},"message":"ok"}
+	 * {"status":true,"code":200,"data":{"current_page":1,"data":[{"id":3,"from_id":0,"to_id":1,"text_id":3,"type":"remind","target_type":"remind_article","target_id":4,"target_url":"","is_read":2,"created_at":"1970-01-01 08:33:41","updated_at":"1970-01-01 08:33:41","deleted_at":null,"type_text":"系统通知","target_type_text":"文章通知","text":{"id":3,"title":"","content":"您的文章审核未通过，原因：111","created_at":"1970-01-01 08:33:41","updated_at":"1970-01-01 08:33:41"}}],"first_page_url":"\/?=1","from":1,"last_page":3,"last_page_url":"\/?=3","next_page_url":"\/?=2","path":"\/","per_page":"1","prev_page_url":null,"to":1,"total":3},"message":"ok"}
 	 **/
 	public function index(Request $request)
 	{
