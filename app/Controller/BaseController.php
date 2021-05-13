9f861849346f46270ec8774d64385ed5fd5dcfe3
凌乐 <563384868@qq.com>
2021-05-13 14:16:08 +0800

    验证报错基类

--- app/Controller/BaseController.php
+++ app/Controller/BaseController.php
@@ -1,13 +1,22 @@
 <?php
+
 /**
- * @author donknap
- * @date 19-10-10 下午2:07
+ * WeEngine Document System
+ *
+ * (c) We7Team 2019 <https://www.w7.cc>
+ *
+ * This is not a free software
+ * Using it under the license terms
+ * visited https://www.w7.cc for more details
  */
 
 namespace W7\App\Controller;
 
-
+use Illuminate\Validation\Factory;
+use Illuminate\Validation\ValidationException;
 use W7\Core\Controller\ControllerAbstract;
+use W7\Core\Exception\ValidatorException;
+use W7\Http\Message\Server\Request;
 
 class BaseController extends ControllerAbstract
 {
@@ -19,5 +28,37 @@ class BaseController extends ControllerAbstract
 			'data' => $data,
 			'message' => $message,
 		];
+	}
+
+	public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
+	{
+		if (empty($request)) {
+			throw new ValidatorException('Request object not found');
+		}
+		$requestData = array_merge([], $request->getQueryParams(), $request->post(), $request->getUploadedFiles());
+		return $this->validateMsg($requestData, $rules, $messages, $customAttributes);
+	}
+
+	protected function validateMsg(array $data, array $rules, array $messages = [], array $customAttributes = [])
+	{
+		try {
+			/**
+			 * @var Factory $validate
+			 */
+			$validate = ivalidator();
+			$result = $validate->make($data, $rules, $messages, $customAttributes)
+				->validate();
+		} catch (ValidationException $e) {
+			$errorMessage = [];
+			$errors = $e->errors();
+			foreach ($errors as $field => $message) {
+				$errorMessage[] = str_replace(['。', ' '], '', $message[0]);
+			}
+			//反馈去重
+			$errorMessage = array_unique($errorMessage);
+			throw new ValidatorException(implode('；', $errorMessage), 403);
+		}
+
+		return $result;
 	}
 }
