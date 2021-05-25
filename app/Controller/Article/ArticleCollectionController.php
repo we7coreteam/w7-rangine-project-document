8203526a8bd1316909719b984f768012958bfcdf
马一帆 <codecola@126.com>
2021-05-25 15:06:53 +0800

    收藏文档列表接口增加标签信息

--- app/Controller/Article/ArticleCollectionController.php
+++ app/Controller/Article/ArticleCollectionController.php
@@ -29,8 +29,8 @@ class ArticleCollectionController extends BaseController
 	 * @api {get} article/articleCollection/all 收藏文章-获取收藏文章列表
 	 * @apiName index
 	 * @apiGroup articleCollectionAdmin
-     *
-     * @apiParam {Number} user_id 用户id
+	 *
+	 * @apiParam {Number} user_id 用户id
 	 *
 	 * @apiSuccess {Number} column_id 栏目ID
 	 * @apiSuccess {Array} tag_ids 标签列表
@@ -48,22 +48,24 @@ class ArticleCollectionController extends BaseController
 	 * @apiSuccess {String} time_str 格式化时间
 	 * @apiSuccess {Object} user 作者信息
 	 * @apiSuccess {String} user.username 作者昵称
+	 * @apiSuccess {Object} tags 标签信息
+	 * @apiSuccess {String} tags.tag_config.name 标签名称
 	 *
 	 * @apiSuccessExample {json} Success-Response:
 	 *{"status":true,"code":200,"data":{"current_page":1,"data":[{"id":16,"column_id":1,"tag_ids":["4","5","6","7"],"user_id":1,"title":"小白兔白又白","content":"<p><span style=\"color: rgb(51, 51, 51); font-family: &quot;Microsoft Yahei&quot;; font-size: 14px; white-space: pre-wrap; background-color: rgb(255, 255, 255);\">小白兔白又白，两只耳朵竖起来，\n\n爱吃萝卜爱吃菜，蹦蹦跳跳真可爱。\n\n小白兔白又白，两只耳朵竖起来，\n\n爱吃萝卜爱吃菜，蹦蹦跳跳真可爱<\/span><\/p>","comment_status":1,"is_reprint":0,"reprint_url":"","home_thumbnail":1,"read_num":4,"praise_num":1,"collection_num":1,"status":1,"reason":"","created_at":"1619321936","updated_at":"1621333349","time_str":"2021-04-25 11:38","status_text":"审核通过","user":{"id":1,"username":"admin","avatar":"https:\/\/wikidev-1257227245.cos.ap-shanghai.myqcloud.com\/document\/AsU43YDSTlhxdSInHsEhcc0603ec6f3Y.jpg","remark":"root","is_ban":0,"group_id":1,"company":"宿州市微擎云计算有限公司","resume":"计算机四级阿萨德阿萨德阿撒","skill":"微擎开发者，dz开发者.","address":"合肥","created_at":"1569409778","updated_at":"1621402909"},"pivot":{"user_id":1,"article_id":16}}],"first_page_url":"\/?page=1","from":1,"last_page":1,"last_page_url":"\/?page=1","next_page_url":null,"path":"\/","per_page":10,"prev_page_url":null,"to":1,"total":1},"message":"ok"}
 	 */
 	public function index(Request $request)
 	{
-        $param = $this->validate($request,[
-            'user_id' => 'required|integer'
-        ],[
-            'user_id' => '用户id'
-        ]);
+		$param = $this->validate($request, [
+			'user_id' => 'required|integer'
+		], [
+			'user_id' => '用户id'
+		]);
 		$page = $request->query('page', 1);
 		$pageSize = intval($request->input('page_size', 10));
 		$user = User::find($param['user_id']);
-		$collectionArticles = $user->articleCollections()->where('article_collection.status', 1)->with('user')->paginate($pageSize, ['*'], 'page', $page)->toArray();
-        $result = ArticleLogic::instance()->getListFirstImg($collectionArticles);
+		$collectionArticles = $user->articleCollections()->where('article_collection.status', 1)->with(['tags','user'])->paginate($pageSize, ['*'], 'page', $page)->toArray();
+		$result = ArticleLogic::instance()->getListFirstImg($collectionArticles);
 		return $this->data($result);
 	}
 
