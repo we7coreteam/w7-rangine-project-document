74e5614bc8aa858ea9f2fc0d79e55b1043f0cd51
凌乐 <563384868@qq.com>
2021-05-27 18:38:23 +0800

    删除命令

--- app/Command/Todo/CustomCommand.php
+++ app/Command/Todo/CustomCommand.php
@@ -12,12 +12,7 @@
 
 namespace W7\App\Command\Todo;
 
-use W7\App\Model\Entity\Article\Article;
-use W7\App\Model\Entity\Message\Message;
-use W7\App\Model\Entity\Message\Text;
 use W7\Console\Command\CommandAbstract;
-use W7\App\Model\Entity\User;
-use Illuminate\Support\Str;
 
 class CustomCommand extends CommandAbstract
 {
@@ -31,58 +26,6 @@ class CustomCommand extends CommandAbstract
 	protected function handle($options)
 	{
 		go(function () {
-			$this->handleRepeatUsername();
-			$this->handleMessageTime();
-			$this->handleMessageContent();
-			$this->output->success('success');
 		});
-	}
-
-	// 修改重复用户名
-	public function handleRepeatUsername()
-	{
-		$users = User::whereIn('username', function ($query) {
-			$query->select('username')->from('user')->groupBy('username')->havingRaw('COUNT(username) > 1');
-		})->get();
-		$users->map(function ($item) {
-			$item->username = $item->username . Str::random(6);
-			$item->save();
-		});
-		return $users;
-	}
-
-	// 修改消息通知时间数据
-	public function handleMessageTime()
-	{
-		$messages = Message::where([
-			['target_type', '=', 'remind_article'],
-			['created_at', '=', 2021],
-			['updated_at', '=', 2021]
-		])->get();
-		$messages->map(function ($itme) {
-			$time = 1621590891;
-			$article = Article::where('id', $itme->target_id)->first();
-			if ($article) {
-				$time = $article->updated_at;
-			}
-			$itme->created_at = $itme->updated_at = $itme->text->created_at = $itme->text->updated_at = $time;
-			$itme->save();
-			$itme->text->save();
-		});
-		return $messages;
-	}
-
-	// 消息通知内容添加识别标签
-	public function handleMessageContent()
-	{
-		$texts = Text::get();
-		$texts->map(function ($item) {
-			$content = $item->content;
-			$start = mb_strpos($content, '《');
-			$end = mb_strripos($content, '》') + 1;
-			$item->content = mb_substr($content, 0, $start) . "<span class='article_title'>" . mb_substr($content, $start, $end - $start) . '</span>' . mb_substr($content, $end);
-			$item->save();
-		});
-		return $texts;
 	}
 }
