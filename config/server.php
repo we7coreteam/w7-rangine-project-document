609e1ef8b78648913a5b8e883865cb478a65ed6d
马一帆 <codecola@126.com>
2021-09-15 11:53:58 +0800

    修改pname

--- config/server.php
+++ config/server.php
@@ -6,7 +6,7 @@
 
 $serverSetting = [
 	'common' => [
-		'pname' => 'document_mall',
+		'pname' => 'document_open_source',
 		'pid_file' => '/tmp/swoole.pid',
 		'max_request' => 10000,
 		'worker_num' => ienv('SERVER_COMMON_WORKER_NUM', 2),
